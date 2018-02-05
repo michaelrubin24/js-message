2ecf7d9300843ebd7d7f658da711243edb08070e
Michael Rubinshtein <deliverypoint7@yandex.ru>
2018-02-05 23:00:09 +0300

    add autoscroll for new messages, deleting for selected messages and analog of button shift

--- scripts/keyboard_script.js
+++ scripts/keyboard_script.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function(){
 var btn_iphone = document.getElementsByClassName('btn-iphone');
 var btn_android = document.getElementsByClassName('btn-android');
 var iphone_text_message = document.getElementsByClassName( 'iphone-text-message')[0];
@@ -19,14 +19,21 @@ var send_message_iphone =  $('.iphone-message-btn')[0];
 var enter_android = $('.enter-android')[0];
 var enter_iphone = $('.enter-iphone')[0];
 
-function fillTextArea(btn, text_message){
+var upper_iphone = $('.upper-case-iphone');
+var upper_android = $('.upper-case-android');
+
+// console.log(upper);
+function fillTextArea(btn, text_message, upper){
   for (var i = 0; i < btn.length; i++) {
     btn[i].addEventListener('click', function(){
       if(this.innerText == 'Пробел'){
         text_message.value += " ";
+      }else if(upper.hasClass("active-upper")){
+        console.log(6);
+        text_message.value += this.innerText.toUpperCase();
+        console.log(this);
       }else{
         text_message.value += this.innerText;
-        // console.dir($('.iphone-text-message'));
       }
     })
   }
@@ -40,11 +47,18 @@ function createMessage(input_text, messages_block_android, messages_block_iphone
   message.innerHTML = input_text.val();
   if(message.innerHTML.length > 1){
     message_wrapper.appendChild(message);
-    messages_block_android.appendChild(message_wrapper.cloneNode(true));
+    var message_wrapper_android = message_wrapper.cloneNode(true);
+    messages_block_android.appendChild(message_wrapper_android);
     messages_block_iphone.appendChild(message_wrapper);
+    input_text.val("");
+    message_wrapper.scrollIntoView(top);
+    message_wrapper_android.scrollIntoView(top);
     message_wrapper.addEventListener('dblclick', function(){
       this.remove();
-    })
+    });
+    message_wrapper_android.addEventListener('dblclick', function(){
+      this.remove();
+    });
   }else{
     input_text.val("мало символов :)");
   }
@@ -53,6 +67,7 @@ function createMessage(input_text, messages_block_android, messages_block_iphone
 function callCreateMessage(){
   send_message_iphone.addEventListener('click', function(){
       createMessage(input_text_iphone, messages_android, messages_iphone, message_wrapper_iphone, message_iphone);
+
   }),
   send_message_android.addEventListener('click', function(){
       createMessage(input_text_android, messages_android, messages_iphone, message_wrapper_android, message_android);
@@ -87,9 +102,18 @@ function clearTextArea(){
     }
   })
 }
-    // message_wrapper.scrollIntoView(top);
-fillTextArea(btn_iphone, iphone_text_message);
-fillTextArea(btn_android, android_text_message);
 
+function toggleUpperCase(upper_iphone, upper_android){
+  upper_iphone.click(function(){
+    upper_iphone.toggleClass('active-upper');
+  });
+  upper_android.click(function(){
+    upper_android.toggleClass('active-upper');
+  });
+}
+toggleUpperCase(upper_iphone, upper_android);
+fillTextArea(btn_iphone, iphone_text_message, upper_iphone);
+fillTextArea(btn_android, android_text_message, upper_android);
 callCreateMessage();
 clearTextArea();
+});
